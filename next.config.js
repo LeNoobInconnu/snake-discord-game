bd03d40eafe3f933025418ec4c09300f287a1e9d
LeNoobInconnu <black.panter.lt800@gmail.com>
2025-07-06 15:17:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'ALLOWALL',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: "frame-ancestors 'self' https://discord.com",
+        },
+      ],
+    },
+  ],
 }
 
 module.exports = nextConfig
