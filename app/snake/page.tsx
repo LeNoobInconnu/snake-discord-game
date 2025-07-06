e033372352e440efae0459c25a66eacd2f0b32b5
LeNoobInconnu <black.panter.lt800@gmail.com>
2025-07-06 15:19:18 +0200

    Create page.tsx

---
+++ app/snake/page.tsx
@@ -0,0 +1,8 @@
+export default function SnakeGame() {
+  return (
+    <main style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🐍 Bienvenue dans le jeu Snake !</h1>
+      <p>Le jeu arrive bientôt...</p>
+    </main>
+  );
+}
